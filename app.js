205b9eeba2206a12e1b564a15bc8169bddc3a847
amrish000 <43074635+amrish000@users.noreply.github.com>
2018-10-02 14:03:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ import React , { Component } from 'react';
 import { Image } from 'react-native';
 
 
-export default class NKCBCLogo extends Component {
- let logo = {
-   uri = 'http://www.nkcbusinesscouncil.com/wp-content/uploads/2017/04/logo1.jpg'
- };
+
 
 
